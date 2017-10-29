2f9a9ec086b1b17ff6778fdbf4ded36d116d5981
Thanapat Sorralump <thanapat423@gmail.com>
2017-10-29 17:52:51 +0700

    :bar_chart: Write Route to Test GET And POST API

--- models/team.js
+++ models/team.js
@@ -1,3 +1,19 @@
 const mongoose = require('mongoose')
 
+const {Mission} = require('./mission')
 
+const teamSchema = mongoose.Schema({
+  teamName: {
+    type: String,
+    require: true
+  },
+  teamImage: {
+    type: String,
+    require: true
+  },
+  mission: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Mission' }]
+})
+
+const Team = mongoose.model('Team', teamSchema)
+
+module.exports = { Team }
--- routes/index.js
+++ routes/index.js
@@ -2,27 +2,51 @@ const express = require("express")
 const router = express.Router()
 
 const { Mission } = require("../models/mission")
+const { Team } = require("../models/team")
 
 router.get("/", function(req, res, next) {
   res.send({ res: "welcome to APIs" })
 })
 
-// router.post("/", (req, res) => {
-//   let mission = new Mission({
-//     missionDesc: req.body.missionDesc,
-//     missionImage: req.body.missionImage,
-//     missionCondition: req.body.missionCondition,
-//     reward: req.body.reward
-//   })
+router.post("/", (req, res) => {
+  let mission = new Mission({
+    missionDesc: req.body.missionDesc,
+    missionImage: req.body.missionImage,
+    missionCondition: req.body.missionCondition,
+    reward: req.body.reward
+  })
 
-//   mission.save().then(
-//     data => {
-//       res.send({
-//         data,
-//         message: "Save Done"
-//       })
-//     }
-//   )
-// })
+  mission.save().then(
+    data => {
+      res.send({
+        data,
+        message: "Save Done"
+      })
+    }
+  )
+})
+
+router.post("/team", (req, res) => {
+  let team = new Team({
+    teamName: req.body.teamName,
+    teamImage: req.body.teamImage,
+    mission: req.body.mission
+  })
+
+  team.save().then(
+    data => {
+      res.send({
+        data,
+        message: "team save done"
+      })
+    }
+  )
+})
+
+router.get("/team", (req, res) => {
+  Team.find().then((team) => {
+      res.send({team})
+  })
+})
 
 module.exports = router
